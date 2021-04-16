4a98a064ca7a6ef1c9b1af02b95f7113ed51f325
Dino Papalia <dinopapalia@Dinos-MacBook-Pro.local>
2021-04-16 14:32:03 -0400

    final

--- js/app.js
+++ js/app.js
@@ -16,14 +16,9 @@ $.ajax("https://spreadsheets.google.com/feeds/list/1_gF9Jwz71nK6o8FToZKJ7h-pNMTr
         }
     })
     console.log(projects)
-    /////////////////////////////////////////////////
-    // jQuery to render your projects below
-    /////////////////////////////////////////////////
-    /////////////////////////////////////////////////
-    ////////////////////////////////////////////////
+
 
     projects.forEach(prj => {
-        // console.log(prj.project)
         
         let $projectCard = $(`
             <div class="col-sm-12 col-md-6 col-lg-3">
@@ -42,38 +37,3 @@ $.ajax("https://spreadsheets.google.com/feeds/list/1_gF9Jwz71nK6o8FToZKJ7h-pNMTr
         $('div.row').append($projectCard)
     })
 })
-
-
-// const menuBtn = document.querySelector('.menu-btn');
-// let menuOpen = false;
-// menuBtn.addEventListener('click', () => {
-//     if (!menuOpen) {
-//         menuBtn.classList.add('open');
-//         menuOpen = true;
-//     } else {
-//         menuBtn.classList.remove('open');
-//         menuOpen = false;
-//     }
-// });
-// menuBtn.addEventListener('click', () => {
-//     if (menuOpen) {
-//         $(".main-menu").css("display", "block")
-//         $(".main-menu, .main-menu li a").css("font-size", "20px")
-//         $(".main-menu, .main-menu li a").css("background-color", "blue")
-//         $(".main-menu, .main-menu li a").css("width", "100%")
-//         $(".main-menu, .main-menu li a").css("text-align", "center")
-//         $(".main-menu, .main-menu li a").css("list-style-type", "none")
-//         $(".main-menu, .main-menu li a").css("text-decoration", "none")
-//         $(".main-menu, .main-menu li a").css("position", "relative")
-//         $(".main-menu, .main-menu li a").css("color", "red")
-//         $(".main-menu, .main-menu li a").css("z-index", "3")  
-//     } else {
-//         $(".main-menu, .main-menu li a").css("display", "none")
-//     }
-// })
-
-
-//.catch in case of an error
-// .catch((error) => {
-//     console.error(error)
-// })
